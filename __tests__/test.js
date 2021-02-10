8e4fb899e899ccffbd5ba5270de261a6188986e2
Svetlana Polozova <svetabjs@gmail.com>
2021-02-10 17:56:31 +0300

    little fixes

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,14 +1,17 @@
+import fs from 'fs';
 import getDiff from '../src/index';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as path from 'path';
-import fs from 'fs';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename); //нужна для составления пути к файлам с фикстурами
-const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8'); //нужна для чтения ожидаемых результатов
-//const json = await readFile('expected.json');
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename); 
+// нужна для составления пути к файлам с фикстурами
+// const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+// нужна для чтения ожидаемых результатов
+// const json = await readFile('expected.json');
   
 const fileBefore = getFixturePath('json1.json');
 const fileAfter = getFixturePath('json2.json');
