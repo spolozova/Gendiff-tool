3854736706628dc888fdfdc4fb44504a9296d44d
Svetlana Polozova <svetabjs@gmail.com>
2021-03-11 13:21:07 +0300

    changed with status root

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,21 +7,24 @@ const formatValue = (value) => {
   return (typeof value === 'string') ? `'${value}'` : value;
 };
 
-const states = {
+const handlers = {
+  root: (ancestor, node) => {
+    const output = node.children
+      .flatMap((child) => {
+        const { key, status } = child;
+        const newAncestor = _.concat(ancestor, key);
+        return handlers[status](newAncestor, child);
+      });
+    return output.join('\n');
+  },
+  node: (ancestor, node) => handlers.root(ancestor, node),
   unchanged: () => [],
   updated: (ancestor, node) => `Property '${ancestor.join('.')}' was updated. From ${formatValue(node.value2)} to ${formatValue(node.value)}`,
   added: (ancestor, node) => `Property '${ancestor.join('.')}' was added with value: ${formatValue(node.value)}`,
   deleted: (ancestor) => `Property '${ancestor.join('.')}' was removed`,
-  node: (ancestor, node, iter) => node.children.flatMap((child) => iter(child, ancestor)),
 };
 
-const getPlain = (difference) => {
-  const iter = (node, ancestor) => {
-    const { key, status } = node;
-    const newAncestor = _.concat(ancestor, key);
-    return states[status](newAncestor, node, iter);
-  };
-  return difference.flatMap((node) => iter(node, [])).join('\n');
+export default (difference) => {
+  const iter = (node, ancestor) => handlers[node.status](ancestor, node);
+  return iter(difference, []);
 };
-
-export default getPlain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,24 +2,26 @@ import _ from 'lodash';
 
 const makeIdent = (depth) => '    '.repeat(depth);
 
-const stringify = (value, identCounter) => {
-  const iter = (currentValue, depth) => {
-    if (!_.isPlainObject(currentValue)) {
-      return currentValue;
-    }
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => `${makeIdent(depth + 1)}${key}: ${iter(val, depth + 1)}`);
-    return [
-      '{',
-      ...lines,
-      `${makeIdent(depth)}}`,
-    ].join('\n');
-  };
-  return iter(value, identCounter);
+const stringify = (value, depth) => {
+  if (!_.isPlainObject(value)) {
+    return value;
+  }
+  const lines = Object.entries(value)
+    .map(([key, val]) => `${makeIdent(depth + 1)}${key}: ${stringify(val, depth + 1)}`);
+  return [
+    '{',
+    ...lines,
+    `${makeIdent(depth)}}`,
+  ].join('\n');
 };
 
-const states = {
+const handlers = {
+  root: (node, depth) => {
+    const output = node.children
+      .flatMap((child) => handlers[child.status](child, depth));
+    return ['{', ...output, `${makeIdent(depth)}}`].join('\n');
+  },
+  node: (node, depth) => `${makeIdent(depth)}    ${node.key}: ${handlers.root(node, depth + 1)}`,
   unchanged: (node, depth) => `${makeIdent(depth)}    ${node.key}: ${stringify(node.value, depth + 1)}`,
   added: (node, depth) => `${makeIdent(depth)}  + ${node.key}: ${stringify(node.value, depth + 1)}`,
   deleted: (node, depth) => `${makeIdent(depth)}  - ${node.key}: ${stringify(node.value, depth + 1)}`,
@@ -27,15 +29,9 @@ const states = {
     `${makeIdent(depth)}  - ${node.key}: ${stringify(node.value2, depth + 1)}`,
     `${makeIdent(depth)}  + ${node.key}: ${stringify(node.value, depth + 1)}`,
   ],
-  node: (node, depth, iter) => `${makeIdent(depth)}    ${node.key}: ${iter(node.children, depth + 1)}`,
 };
 
-const getStylish = (difference) => {
-  const iter = (currentValue, depth) => {
-    const stylizedDiff = currentValue
-      .flatMap((node) => states[node.status](node, depth, iter));
-    return ['{', ...stylizedDiff, `${makeIdent(depth)}}`].join('\n');
-  };
+export default (difference) => {
+  const iter = (node, depth) => handlers[node.status](node, depth);
   return iter(difference, 0);
 };
-export default getStylish;
