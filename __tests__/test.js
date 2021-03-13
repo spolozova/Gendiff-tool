1581ed1e7c18f8acab183c88cdee620379ee5800
Svetlana Polozova <svetabjs@gmail.com>
2021-03-13 11:07:12 +0300

    apdated tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,142 +2,52 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as path from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const jsonBefore = getFixturePath('json1.json');
-const jsonAfter = getFixturePath('json2.json');
-const yamlBefore = getFixturePath('yaml1.yml');
-const yamlAfter = getFixturePath('yaml2.yml');
+const expectedData = { stylish: [], plain: [], json: [] };
+const json1 = getFixturePath('json1.json');
+const json2 = getFixturePath('json2.json');
+const yaml1 = getFixturePath('yaml1.yml');
+const yaml2 = getFixturePath('yaml2.yml');
+beforeAll(() => {
+const stylishData = fs.readFileSync(getFixturePath('stylish.txt'), 'utf-8');
+const plainData = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
+const jsonData = fs.readFileSync(getFixturePath('json.txt'), 'utf-8').trim();
+expectedData.stylish = stylishData.split('\n\n\n');
+expectedData.plain = plainData.split('\n\n\n');
+expectedData.json = jsonData.trim().split('\n\n\n');
+});
 
 test('getDiff JSON stylish', () => {
-  expect(genDiff(jsonBefore, jsonAfter, 'stylish')).toEqual(`{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`);
+  const expected = expectedData.stylish[0];
+  expect(genDiff(json1, json2, 'stylish')).toEqual(expected);
 });
 
 test('genDiff YAML stylish', () => {
-  expect(genDiff(yamlBefore, yamlAfter, 'stylish')).toEqual(`{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`);
+  const expected = expectedData.stylish[1];
+  expect(genDiff(yaml1, yaml2, 'stylish')).toEqual(expected);
 });
 
 test('getDiff JSON plain', () => {
-  expect(genDiff(jsonBefore, jsonAfter, 'plain')).toEqual(`Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`);
+  const expected = expectedData.plain[0];
+  expect(genDiff(json1, json2, 'plain')).toEqual(expected);
 });
 
 test('genDiff YAML plain', () => {
-  expect(genDiff(yamlBefore, yamlAfter, 'plain')).toEqual(`Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`);
+  const expected = expectedData.plain[1];
+  expect(genDiff(yaml1, yaml2, 'plain')).toEqual(expected);
 });
 
 test('getDiff JSON json', () => {
-  expect(genDiff(jsonBefore, jsonAfter, 'json')).toEqual('{"status":"root","children":[{"key":"common","children":[{"key":"follow","value":false,"status":"added"},{"key":"setting1","value":"Value 1","status":"unchanged"},{"key":"setting2","value":200,"status":"deleted"},{"key":"setting3","value":null,"status":"updated","value2":true},{"key":"setting4","value":"blah blah","status":"added"},{"key":"setting5","value":{"key5":"value5"},"status":"added"},{"key":"setting6","children":[{"key":"doge","children":[{"key":"wow","value":"so much","status":"updated","value2":""}],"status":"node"},{"key":"key","value":"value","status":"unchanged"},{"key":"ops","value":"vops","status":"added"}],"status":"node"}],"status":"node"},{"key":"group1","children":[{"key":"baz","value":"bars","status":"updated","value2":"bas"},{"key":"foo","value":"bar","status":"unchanged"},{"key":"nest","value":"str","status":"updated","value2":{"key":"value"}}],"status":"node"},{"key":"group2","value":{"abc":12345,"deep":{"id":45}},"status":"deleted"},{"key":"group3","value":{"deep":{"id":{"number":45}},"fee":100500},"status":"added"}]}');
+  const expected = expectedData.json[0];
+  expect(genDiff(json1, json2, 'json')).toEqual(expected);
 });
 
 test('genDiff YAML json', () => {
-  expect(genDiff(yamlBefore, yamlAfter, 'json')).toEqual('{"status":"root","children":[{"key":"common","children":[{"key":"follow","value":false,"status":"added"},{"key":"setting1","value":"Value 1","status":"unchanged"},{"key":"setting2","value":200,"status":"deleted"},{"key":"setting3","value":null,"status":"updated","value2":true},{"key":"setting4","value":"blah blah","status":"added"},{"key":"setting5","value":{"key5":"value5"},"status":"added"},{"key":"setting6","children":[{"key":"doge","children":[{"key":"wow","value":"so much","status":"updated","value2":""}],"status":"node"},{"key":"key","value":"value","status":"unchanged"},{"key":"ops","value":"vops","status":"added"}],"status":"node"}],"status":"node"},{"key":"group1","children":[{"key":"baz","value":"bars","status":"updated","value2":"bas"},{"key":"foo","value":"bar","status":"unchanged"},{"key":"nest","value":"str","status":"updated","value2":{"key":"value"}}],"status":"node"},{"key":"group2","value":{"abc":12345,"deep":{"id":45}},"status":"deleted"},{"key":"group3","value":{"deep":{"id":{"number":45}},"fee":100500},"status":"added"}]}');
+  const expected = expectedData.json[1];
+  expect(genDiff(yaml1, yaml2, 'json')).toEqual(expected);
 });
