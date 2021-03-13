868c02cc07256055325a168162b35512d68cfcbe
Svetlana Polozova <svetabjs@gmail.com>
2021-03-13 11:28:20 +0300

    fix tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -8,19 +8,17 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const expectedData = { stylish: [], plain: [], json: [] };
 const json1 = getFixturePath('json1.json');
 const json2 = getFixturePath('json2.json');
 const yaml1 = getFixturePath('yaml1.yml');
 const yaml2 = getFixturePath('yaml2.yml');
-beforeAll(() => {
-  const stylishData = fs.readFileSync(getFixturePath('stylish.txt'), 'utf-8');
-  const plainData = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
-  const jsonData = fs.readFileSync(getFixturePath('json.txt'), 'utf-8').trim();
-  expectedData.stylish = stylishData.split('\n\n\n');
-  expectedData.plain = plainData.split('\n\n\n');
-  expectedData.json = jsonData.trim().split('\n\n\n');
-});
+const stylishData = fs.readFileSync(getFixturePath('stylish.txt'), 'utf-8');
+const plainData = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
+const jsonData = fs.readFileSync(getFixturePath('json.txt'), 'utf-8');
+const stylish = stylishData.split('\n\n\n');
+const plain = plainData.split('\n\n\n');
+const json = jsonData.trim().split('\n\n\n');
+const expectedData = { stylish, plain, json };
 
 test('getDiff JSON stylish', () => {
   const expected = expectedData.stylish[0];
